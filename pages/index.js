f6c1302419e4aaef82b7189ec937afc7e5783505
thiagoHagge <thiagohagge09@gmail.com>
2021-01-26 20:22:08 -0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,43 @@
 import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import db from '../db.json';
+
+import Widget from '../src/components/Widget';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizBackground from '../src/components/QuizBackground';
+
+const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+          <h1>CSS</h1>
+          </Widget.Header>
+          <Widget.Content>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <h1>Quizes da galera</h1>
+          <p>Lorem ipsum dolor sit</p>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/thiagoHagge"/>
+    </QuizBackground>
+  );
 }
